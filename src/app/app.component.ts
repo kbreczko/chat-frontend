6dd766921743f423c5cc09371a0a297f0f98f7e1
kbreczko <kamil.breczko@gmail.com>
2019-01-27 18:41:14 +0100

    add support for received messages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import {Component} from '@angular/core';
 import {MessageData} from './modules';
+import {environment} from '../environments/environment';
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
+import * as Uuid from 'uuid';
 
 @Component({
   selector: 'app-root',
@@ -7,13 +11,26 @@ import {MessageData} from './modules';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  private stompClient;
   public message: MessageData;
   public messages: MessageData[];
 
   constructor() {
-    this.message = new MessageData('', 'example', new Date());
+    this.initializeWebSocketConnection();
+    this.message = new MessageData('', 'guest' + Uuid.v4(), new Date());
     this.messages = [
-      new MessageData('Welcome to chatbot universe', 'bot', new Date())
+      new MessageData('Welcome to chat universe', 'bot', new Date())
     ];
+  }
+
+  private initializeWebSocketConnection() {
+    const socket = new SockJS(environment.backend + '/socket');
+    this.stompClient = Stomp.over(socket);
+    const that = this;
+    this.stompClient.connect(
+      {},
+      () => that.stompClient.subscribe('/chat', (message) => that.messages.push(JSON.parse(message.body))),
+      (error) => console.log(`error: ${error}`)
+    );
   }
 }
